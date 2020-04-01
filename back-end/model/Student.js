c0fa5617bcc33b75e2f97ab5d6e149bc1a1eb3b6
Ranen-Khlabi <Ranen-Alkhlabi@hotmail.com>
2020-04-01 19:03:50 +0300

    Create user model

---
+++ back-end/model/Student.js
@@ -0,0 +1,14 @@
+// Require necessary NPM packages
+const mongoose = require('mongoose');
+
+
+// Define Article Schema
+const studentSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+});
+
+
+// Compile our Model based on the Schema
+const Student = mongoose.model('Student', studentSchema);
+// Export our Model for use
+module.exports = Student;
