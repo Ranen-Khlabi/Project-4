c816ab3c6897f9ff70fb9495b302e1c3c8cfe451
Ranen-Khlabi <Ranen-Alkhlabi@hotmail.com>
2020-04-01 19:06:53 +0300

    Create Contributor model

---
+++ back-end/model/Contributor.js
@@ -0,0 +1,14 @@
+// Require necessary NPM packages
+const mongoose = require('mongoose');
+
+// Define Article Schema
+const contributorSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+
+});
+
+// Compile our Model based on the Schema
+const Contributor = mongoose.model('Contributor', contributorSchema);
+
+// Export our Model for use
+module.exports = Contributor;
