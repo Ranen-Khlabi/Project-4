61aa79e59d7b2cb65196582d93074254889db361
Ranen-Khlabi <Ranen-Alkhlabi@hotmail.com>
2020-04-01 19:12:06 +0300

    Create Book model

---
+++ back-end/model/Book.js
@@ -0,0 +1,20 @@
+// Require necessary NPM packages
+const mongoose = require('mongoose');
+
+// Define Book Schema
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  photo:{type:String ,default:"https://a.loveholidays.com/react/6f353ee/default-hotel-img.jpg"},
+  description:{ type: String, required: true},
+  link:{type:String, required: true },
+  contributor:  {
+    type: Schema.Types.ObjectId,
+    ref: "Contributor"
+  }
+});
+
+// Compile our Model based on the Schema
+const Book = mongoose.model('Book', bookSchema);
+
+// Export our Model for use
+module.exports = Book;
