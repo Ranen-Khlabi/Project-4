9e73266b20e0979830347fa9f69168bc2871e7b1
Ranen-Khlabi <Ranen-Alkhlabi@hotmail.com>
2020-04-02 20:31:20 +0300

    Add delete user action

--- back-end/routes/student.js
+++ back-end/routes/student.js
@@ -6,6 +6,7 @@ const Student = require("../model/Student")
 const router = express.Router();
 
 
+
 /**
  * @method : GET
  * @route : /api/students
@@ -22,6 +23,7 @@ router.get("/api/students", (req, res) => {
         res.status(500).json({ error: error });
       });
   });
+
 
 
 /**
@@ -67,6 +69,7 @@ router.post("/api/students", (req, res) => {
 });
 
 
+
 /**
  * @method PATCH
  * @route   /api/students/id
@@ -76,26 +79,60 @@ router.post("/api/students", (req, res) => {
 router.patch("/api/students/:id", (req, res) => {
   // Find the student with the passed ID
   Student.findById(req.params.id)
-      .then(student => {
-          // Check if a student is found by the passed ID
-          if (student) {
-              // Update the existing student with the new data from the request body
-              return student.update(req.body.student);
-          } else {
-              // If no student was found by the passed ID, send an error message as response
-              res.status(404).json({
-                  error: {
-                      name: "DocumentNotFoundError",
-                      message: "The provided ID doesn't match any documents"
-                  }
-              });
+    .then(student => {
+      // Check if a student is found by the passed ID
+      if (student) {
+        // Update the existing student with the new data from the request body
+        return student.update(req.body.student);
+      } else {
+        // If no student was found by the passed ID, send an error message as response
+        res.status(404).json({
+          error: {
+            name: "DocumentNotFoundError",
+            message: "The provided ID doesn't match any documents"
           }
-      })
-      .then(() => {
-          // If the update succeeded, return 204 and no JSON response
-          res.status(204).end();
-      })
-      .catch(error => res.status(500).json({ error }));
+        });
+      }
+    })
+    .then(() => {
+      // If the update succeeded, return 204 and no JSON response
+      res.status(204).end();
+    })
+    .catch(error => res.status(500).json({ error }));
+});
+
+
+
+/**
+ * @method  : delete
+ * @route   : /api/students/id
+ * @action  : Destory
+ * @desc    : delete an student by student ID
+ */
+router.delete("/api/students/:id", (req, res) => {
+  Student.findById(req.params.id)
+    .then(student => {
+      if (student) {
+        //pass the result of mongooes's ".delete" method to thee next ".then"
+        student.remove();
+      } else {
+        // if we coudn;t find a document with matching ID
+        res.status(404).json({
+          error: {
+            name: "DocumentNotFoundError ",
+            message: "The  providednId dosen't match any documents"
+          }
+        });
+      }
+    })
+    //another then
+    .then(() => {
+      // if the deletion succeeded , return 204 and no JSON
+      res.status(204).end();
+    })
+    .catch(error => {
+      res.status.json({ error: error });
+    });
 });
 
 
