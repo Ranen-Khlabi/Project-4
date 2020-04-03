f89bf8f8648aa212c508972e10f665cb976e5847
Ranen-Khlabi <Ranen-Alkhlabi@hotmail.com>
2020-04-03 18:24:21 +0300

    Add link route student and contributor & fix rename file.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import apiURL from "./apiConfig";
 import Contributor from './contributors/components/Contributor';
